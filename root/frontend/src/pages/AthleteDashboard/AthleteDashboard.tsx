ff5c080fcfd101b808c6d4fbe21941240b254df4
jaydenmetcalfe17 <jayden10.9@gmail.com>
2026-04-15 22:13:23 -0700

    Changing how user and athlete ids connect

--- root/frontend/src/pages/AthleteDashboard/AthleteDashboard.tsx
+++ root/frontend/src/pages/AthleteDashboard/AthleteDashboard.tsx
@@ -26,7 +26,7 @@ const AthleteDashboard: React.FC = () => {
     if (user?.status == 'coach') {
       insert = `athleteId=${params.athleteId}`
     } else if (user?.status == 'athlete' || user?.status == 'parent')  {
-      console.log("IN HERE!");
+      console.log("IN HERE! athleteid: ", user?.athleteId);
       insert = `athleteId=${user?.athleteId}`
       setIsVisible(!isVisible);
     }
