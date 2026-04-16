6de1c786b5a56400ff9fbb2f13f54cd10aa4ea94
jaydenmetcalfe17 <jayden10.9@gmail.com>
2026-04-15 22:24:37 -0700

    Changing how user and athlete ids connect

--- root/backend/routes/auth.js
+++ root/backend/routes/auth.js
@@ -53,6 +53,12 @@ router.post('/login', (req, res, next) => {
         email: user.email,
         status: user.status
       };
+
+      if (user.athlete_id != null) {
+        console.log("I am a safe user adding the athleteId");
+        safeUser.athleteId = user.athlete_id;
+      }
+
       console.log("Safe user passed through: ", safeUser);
       return res.json({ success: true, safeUser });
     });
