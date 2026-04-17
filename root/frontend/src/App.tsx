e4de272c00e882babe3c475c476b6554bae067d2
jaydenmetcalfe17 <jayden10.9@gmail.com>
2026-04-16 17:40:19 -0700

    Fixing what is visible to parent profiles

--- root/frontend/src/App.tsx
+++ root/frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import { Navigate, Route, Routes} from 'react-router-dom'
 import './assets/scss/App.scss'
 import MainLayout from './layout/MainLayout/MainLayout'
-import CreateAthletePage from './pages/CreateAthletePage/CreateAthletePage';
-import CreateSessionPage from './pages/CreateSessionPage/CreateSessionPage';
+// import CreateAthletePage from './pages/CreateAthletePage/CreateAthletePage';
+// import CreateSessionPage from './pages/CreateSessionPage/CreateSessionPage';
 import CreateUserPage from './pages/CreateUserPage/CreateUserPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import Dashboard from './layout/Dashboard/Dashboard';
