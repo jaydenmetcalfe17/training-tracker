0a0b55d18067eb3ede062f9c8f439caa1ee9d22f
jaydenmetcalfe17 <jayden10.9@gmail.com>
2026-05-07 08:01:23 -0700

    Preparing to start testing

---
+++ root/backend/tests/database.test.js
@@ -0,0 +1,7 @@
+require('dotenv').config({ path: './.env' }); // adjust path if needed
+
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+
